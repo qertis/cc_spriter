ec9333e327bfae45e29d2aa9148ab816e43463c7
Denis Baskovsky <baskovskiy@bifit.com>
2015-11-25 19:18:46 +0300

    minified script

--- bin/compile.sh
+++ bin/compile.sh
@@ -3,13 +3,13 @@
 
 rm -rf bower_components/temp
 
-# Create dist directory
+# Create dist and temp directories
 mkdir -p dist
 
-# Compile source
-webpack --production
+# Build minification
+webpack -p
 
-cd bower_components
+cd bower_components/
 
 mkdir -p temp
 
@@ -24,14 +24,24 @@ google-closure-library/closure/bin/build/closurebuilder.py \
   --output_mode=compiled \
   --compiler_jar=google-closure-library/compiler.jar > temp/closure_compiled.js
 
-cd temp
+cd temp/
 
 # Concat files
 cat closure_compiled.js \
     cc_spriter_component.js \
-    > ../../dist/cc_spriter_min.js
+    > ../temp/temp_cc_spriter_min.js
+
+# Closure wrap
+# FIXME (Unexpected token ;)
+#((tac < temp_cc_spriter_min.js; echo -n '!function(){')|tac; echo -n '}();') > cc_spriter_min.js
+
+# Move minified file
+#mv cc_spriter_min.js ../../dist/cc_spriter_min.js
+mv temp_cc_spriter_min.js ../../dist/cc_spriter_min.js
 
 # Remove old files
-cd ../..
-sleep 5
-rm -rf bower_components/temp/
+sleep 1
+cd ../../
+rm -rf bower_components/temp
+
+echo 'end'
--- cc_spriter.js
+++ cc_spriter.js
@@ -8,176 +8,179 @@
  * - Jason Andersen jgandersen@gmail.com
  * - Isaac Burns isaacburns@gmail.com
  */
+!function (window, cc, spriter) {
+  'use strict';
 
-'use strict';
-cc.Spriter = cc.Sprite.extend({
-  /* Loading indicator */
-  _ready: false,
+  cc.Spriter = cc.Sprite.extend({
+    /* Loading indicator */
+    _ready: false,
 
-  /* Resources path */
-  sconLink: '',
-  sconPath: '',
+    /* Resources path */
+    sconLink: '',
+    sconPath: '',
 
-  _entity: null,
-  _animation: null,
+    _entity: null,
+    _animation: null,
 
-  data: null,
-  pose: null,
+    data: null,
+    pose: null,
 
-  /**
-   * Spriter constructor
-   * @param {String} sconLink scon file to use for this animation
-   */
-  ctor: function (sconLink) {
-    this._super();
+    /**
+     * Spriter constructor
+     * @param {String} sconLink scon file to use for this animation
+     */
+    ctor: function (sconLink) {
+      this._super();
 
-    this.sconLink = sconLink;
-    this.preload(data => {
-      if (data.error) {
-        throw data.error;
-      }
-      this._ready = true;
-      this.setEntity(this._entity);
-      this.setAnim(this._animation);
-      this.scheduleUpdate();
-    });
-  },
-
-  /**
-   * Set entity
-   * @param entity
-   */
-  setEntity: function (entity) {
-    this._entity = entity;
-
-    if (this._ready) {
-      this.pose.setEntity(entity);
-    }
-  },
-
-  /**
-   * Set animation
-   * @param animation
-   */
-  setAnim: function (animation) {
-    this._animation = animation;
-
-    if (this._ready) {
-      this.pose.setAnim(animation);
-    }
-  },
-
-  /**
-   * Prealod scon resource
-   * @param {function} callback
-   */
-  preload: function (callback) {
-    let sconLink = this.sconLink;
-
-    if (this._ready) {
-      return callback({
-        error: 'уже установлено'
+      this.sconLink = sconLink;
+      this.preload(data => {
+        if (data.error) {
+          throw data.error;
+        }
+        this._ready = true;
+        this.setEntity(this._entity);
+        this.setAnim(this._animation);
+        this.scheduleUpdate();
       });
-    }
+    },
 
-    cc.loader.loadJson(sconLink, (error, scon) => {
-      if (error) {
-        return callback({error});
+    /**
+     * Set entity
+     * @param entity
+     */
+    setEntity: function (entity) {
+      this._entity = entity;
+
+      if (this._ready) {
+        this.pose.setEntity(entity);
+      }
+    },
+
+    /**
+     * Set animation
+     * @param animation
+     */
+    setAnim: function (animation) {
+      this._animation = animation;
+
+      if (this._ready) {
+        this.pose.setAnim(animation);
+      }
+    },
+
+    /**
+     * Prealod scon resource
+     * @param {function} callback
+     */
+    preload: function (callback) {
+      let sconLink = this.sconLink;
+
+      if (this._ready) {
+        return callback({
+          error: 'уже установлено'
+        });
       }
 
-      let sconPath = scon.sconPath = sconLink.replace(/\w+.scon$/, '');
-      let loaderIndex = 0;
+      cc.loader.loadJson(sconLink, (error, scon) => {
+        if (error) {
+          return callback({error});
+        }
 
-      let data = this.data = new spriter.Data().load(scon); // create and load Spriter data from SCON file
-      let pose = this.pose = new spriter.Pose(data); // create Spriter pose and attach data
+        let sconPath = scon.sconPath = sconLink.replace(/\w+.scon$/, '');
+        let loaderIndex = 0;
 
-      /* Getting file count */
-      scon.folder.forEach(folder => folder.file.forEach(() => ++loaderIndex));
+        let data = this.data = new spriter.Data().load(scon); // create and load Spriter data from SCON file
+        let pose = this.pose = new spriter.Pose(data); // create Spriter pose and attach data
 
-      data.folder_array.forEach(folder => {
-        folder.file_array.forEach(file => {
+        /* Getting file count */
+        scon.folder.forEach(folder => folder.file.forEach(() => ++loaderIndex));
 
-          switch (file.type) {
-            case 'image':
-              let image_key = file.name;
-              let fileUrl = sconPath + file.name;
+        data.folder_array.forEach(folder => {
+          folder.file_array.forEach(file => {
 
-              cc.loader.loadImg(fileUrl, (error, img) => {
-                if (error) {
-                  return callback({error});
-                }
+            switch (file.type) {
+              case 'image':
+                let image_key = file.name;
+                let fileUrl = sconPath + file.name;
 
-                let texture = new cc.Texture2D();
-                texture.initWithElement(img);
+                cc.loader.loadImg(fileUrl, (error, img) => {
+                  if (error) {
+                    return callback({error});
+                  }
 
-                let spriteFrame = new cc.SpriteFrame();
-                spriteFrame.setTexture(texture);
+                  let texture = new cc.Texture2D();
+                  texture.initWithElement(img);
 
-                let rect = cc.rect(0, 0, file.width, file.height);
-                spriteFrame.setRect(rect);
+                  let spriteFrame = new cc.SpriteFrame();
+                  spriteFrame.setTexture(texture);
 
-                cc.spriteFrameCache.addSpriteFrame(spriteFrame, image_key);
+                  let rect = cc.rect(0, 0, file.width, file.height);
+                  spriteFrame.setRect(rect);
 
-                if (--loaderIndex === 0) {
-                  return callback({error: false});
-                }
-              });
-              break;
+                  cc.spriteFrameCache.addSpriteFrame(spriteFrame, image_key);
 
-            case 'sound':
-              break;
+                  if (--loaderIndex === 0) {
+                    return callback({error: false});
+                  }
+                });
+                break;
 
-            default:
-              cc.log("TODO: load", file.type, file.name);
-              break;
-          }
+              case 'sound':
+                break;
+
+              default:
+                cc.log("TODO: load", file.type, file.name);
+                break;
+            }
+          });
         });
       });
-    });
 
-  },
+    },
 
-  /**
-   * Update every tick
-   * @param dt
-   */
-  update: function (dt) {
-    dt = 1000 / 60; // time step in milliseconds
-    this.removeAllChildrenWithCleanup(true);
-    this.pose.update(dt); // accumulate time
-    this.pose.strike(); // process time slice
-    this.pose.object_array.forEach(object => {
-      switch (object.type) {
-        case 'sprite':
-          let folder = this.pose.data.folder_array[object.folder_index];
-          if (!folder) {
-            return;
-          }
-          let file = folder.file_array[object.file_index];
-          if (!file) {
-            return;
-          }
-          let image_key = file.name;
+    /**
+     * Update every tick
+     * @param dt
+     */
+    update: function (dt) {
+      dt = 1000 / 60; // time step in milliseconds
+      this.removeAllChildrenWithCleanup(true);
+      let pose = this.pose;
+      pose.update(dt); // accumulate time
+      pose.strike(); // process time slice
+      pose.object_array.forEach(object => {
+        switch (object.type) {
+          case 'sprite':
+            let folder = pose.data.folder_array[object.folder_index];
+            if (!folder) {
+              return;
+            }
+            let file = folder.file_array[object.file_index];
+            if (!file) {
+              return;
+            }
+            let image_key = file.name;
 
-          let spriteFrame = cc.spriteFrameCache.getSpriteFrame(image_key);
-          if (spriteFrame) {
-            let sprite = new cc.Sprite();
-            sprite.setSpriteFrame(spriteFrame);
-            sprite.setOpacity(object.alpha * 255);
-            sprite.setPositionX(object.world_space.position.x);
-            sprite.setPositionY(object.world_space.position.y);
-            sprite.setScaleX(object.world_space.scale.x);
-            sprite.setScaleY(object.world_space.scale.y);
-            sprite.setRotation(-object.world_space.rotation.deg);
+            let spriteFrame = cc.spriteFrameCache.getSpriteFrame(image_key);
+            if (spriteFrame) {
+              let sprite = new cc.Sprite();
+              sprite.setSpriteFrame(spriteFrame);
+              sprite.setOpacity(object.alpha * 255);
+              sprite.setPositionX(object.world_space.position.x);
+              sprite.setPositionY(object.world_space.position.y);
+              sprite.setScaleX(object.world_space.scale.x);
+              sprite.setScaleY(object.world_space.scale.y);
+              sprite.setRotation(-object.world_space.rotation.deg);
 
-            this.addChild(sprite);
-          }
-          break;
-        case 'entity':
-          cc.log('TODO ');
-          break;
-      }
-    });
+              this.addChild(sprite);
+            }
+            break;
+          case 'entity':
+            cc.log('TODO ');
+            break;
+        }
+      });
 
-  }
-});
+    }
+  });
+}(window, window.cc, window.spriter);
